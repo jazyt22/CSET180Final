2fa6cfd85b3248830c803317122b17343cb1d5a6
jaz <jazlynntroop@gmail.com>
2023-05-08 10:32:06 -0400

    register account

--- main.py
+++ main.py
@@ -1,26 +1,49 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 from sqlalchemy import Column, Integer, String, Numeric, create_engine, text
 
 app = Flask(__name__, template_folder='template')
 
 conn_str = "mysql://root:Wilson02!@localhost/180Final"
 engine = create_engine(conn_str, echo=True)
-connection = engine.connect()
+conn = engine.connect()
+
+
+def p(str):
+    abc = "abcdefghijklmnopqrstuvwxyz"
+    sum = 0
+    for char in str:
+        sum += abc.find(char) + 1
+    return sum
+
 
 @app.route('/')
 def home():
     return render_template('home.html')
+#
+# # Define a function to check if the email already exists in the database
+# def check_email_exists(email):
+#     cursor = conn_str.cursor()
+#     cursor.execute("SELECT * FROM users WHERE email = %s", (email,))
+#     user = cursor.fetchone()
+#     cursor.close()
+#     if user is not None:
+#         return True
+#     else:
+#         return False
+
 
 #regitser account
-@app.route('/reg', methods = ["GET"])
+
+
+@app.route('/register', methods=['GET', 'POST'])
 def create_account():
+    if request.method == 'POST':
+        conn.execute(text("INSERT INTO register VALUES (:name, :email, :username, :password, :userID, :account_type)"),request.form)
+        conn.commit()
     return render_template("register.html")
 
-@app.route('/register', methods = ["POST"])
-def create_account_type():
-    connection.execute(text("INSERT INTO cset160 VALUES (:name, :email, :username, :password, :userID, :account_type)"), request.form)
-    connection.commit()
-    return render_template("register.html")
+
+
 
 
 if __name__ == '__main__':
